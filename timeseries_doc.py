5f022705de2d4883aa9166ba6484adda8da030f2
Bazarovay <rxs6754@rit.edu>
2019-05-16 23:48:08 +0500

    examples

---
+++ timeseries_doc.py
@@ -0,0 +1,11 @@
+# Supplier 	Amount 	Supplier 	Amount
+# 1 	9 	7 	11
+# 2 	8 	8 	7
+# 3 	9 	9 	13
+# 4 	12 	10 	9
+# 5 	9 	11 	11
+# 6 	12 	12 	10
+
+supplier = [x for x in range(1,13)]
+amount = []
+print(supplier)
